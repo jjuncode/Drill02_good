6b1e04b4023c7e458a588a1503b33b33e9bd17a1
jjuncode <jjun.code@gmail.com>
2023-09-20 17:58:04 +0900

    원그리기

--- character_run.py
+++ character_run.py
@@ -6,12 +6,18 @@ open_canvas()
 grass = load_image("grass.png")
 character = load_image("character.png")
 
+
 def run_circle():
     print("Circle")
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(400,90)
-    delay(1)
+
+    cx,cy,r = get_canvas_width()/2, get_canvas_height()/2, 200
+    for deg in range(0,360,5):
+        x = cx+r + math.cos(deg/360 * 2* math.pi)
+        y = cy+r + math.sin(deg/360 * 2* math.pi)
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        delay(0.01)
 
 def run_rectangle():
     print("Rectangle")
