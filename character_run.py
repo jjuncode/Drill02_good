3b659b05228a803fb2241221a18dbfd39930116a
jjuncode <jjun.code@gmail.com>
2023-09-20 17:43:46 +0900

    Circle, Rectangle Func create
    원, 사각형 그리는 함수 생성

--- character_run.py
+++ character_run.py
@@ -3,4 +3,21 @@ import math
 
 open_canvas()
 
+grass = load_image("grass.png")
+character = load_image("character.png")
+
+def run_circle():
+    print("Circle")
+    pass
+
+def run_rectangle():
+    print("Rectangle")
+    pass
+
+while(True):
+    run_circle()
+    run_rectangle()
+    break
+
+
 close_canvas()
